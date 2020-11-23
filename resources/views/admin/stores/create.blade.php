adefc068f1f6f1786891095d6e46db3b1a00e129
Breno Almeida <kbreno08@gmail.com>
2020-11-23 19:49:10 -0300

    Inserção de loja

---
+++ resources/views/admin/stores/create.blade.php
@@ -0,0 +1,42 @@
+<h1>Criar Loja</h1>
+<form action="/admin/stores/store" method="POST">
+    <input type="hidden" name="_token" value="{{csrf_token()}}">
+    <div>
+        <label>Nome da Loja</label>
+        <input type="text" name="name">
+    </div>
+
+    <div>
+        <label>Descrição</label>
+        <input type="text" name="description">
+    </div>
+
+    <div>
+        <label>Telefone</label>
+        <input type="text" name="phone">
+    </div>
+
+    <div>
+        <label>Celular/Whatsapp</label>
+        <input type="text" name="mobile_phone">
+    </div>
+
+    <div>
+        <label>Slug</label>
+        <input type="text" name="slug">
+    </div>
+
+    <div>
+        <label>Usuário</label>
+        <select name="user">
+            @foreach($users as $user)
+
+            <option value="{{$user->id}}"> {{$user->name}} </option>
+
+            @endforeach
+        </select>
+    </div>
+    <div>
+        <button type="submit">Criar Loja</button>
+    </div>
+</form>
