a9e8ca1e558d1703ef6c57688742c292ded6437d
Breno Almeida <kbreno08@gmail.com>
2020-11-23 18:50:20 -0300

    Primeiro commit do projeto

---
+++ routes/web.php
@@ -0,0 +1,28 @@
+<?php
+
+/*
+|--------------------------------------------------------------------------
+| Web Routes
+|--------------------------------------------------------------------------
+|
+| Here is where you can register web routes for your application. These
+| routes are loaded by the RouteServiceProvider within a group which
+| contains the "web" middleware group. Now create something great!
+|
+*/
+
+Route::get('/', function () {
+    return view('welcome');
+});
+
+Route::get('/model', function(){
+    $products = \App\Product::all(); // select * from products
+
+    $user = new \App\User();
+    $user->name = 'Usuário Teste';
+    $user->email = 'email@test.com';
+    $user->password = bcrypt('12345678');
+//    return $user->save();
+
+    return $user->all();
+});
