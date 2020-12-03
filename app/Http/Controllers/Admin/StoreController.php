942504254f74c6542b51642c73ec30a0ca72e52b
Breno Almeida <kbreno08@gmail.com>
2020-12-02 22:20:24 -0300

    Implementação de criação de lojas fazendo uso do usuário logado

--- app/Http/Controllers/Admin/StoreController.php
+++ app/Http/Controllers/Admin/StoreController.php
@@ -7,37 +7,42 @@ use Illuminate\Http\Request;
 
 class StoreController extends Controller
 {
-    public function index(){
+    public function index()
+    {
         $stores = \App\Store::paginate(10);
 
         return view('admin.stores.index', compact('stores'));
     }
 
-    public function create(){
+    public function create()
+    {
         $users = \App\User::all(['id', 'name']);
 
         return view('admin.stores.create', compact('users'));
     }
 
-    public function store(Request $request){
+    public function store(Request $request)
+    {
 
         $data = $request->all();
+        $user = auth()->user();
 
-        $user = \App\User::find($data['user']);
+
         $store = $user->store()->create($data);
 
         flash('Loja criada com sucesso')->success();
         return redirect()->route('admin.stores.index');
-
     }
 
-    public function edit($store){
+    public function edit($store)
+    {
         $store = \App\Store::find($store);
 
         return view('admin.stores.edit', compact('store'));
     }
 
-    public function update(Request $request, $store){
+    public function update(Request $request, $store)
+    {
 
         $data = $request->all();
 
@@ -47,15 +52,14 @@ class StoreController extends Controller
         flash('Loja atualizada com sucesso')->success();
         return redirect()->route('admin.stores.index');
     }
-    
-    public function destroy($store){
-        
+
+    public function destroy($store)
+    {
+
         $store = \App\Store::find($store);
         $store->delete();
-        
+
         flash('Loja removida com sucesso')->success();
         return redirect()->route('admin.stores.index');
     }
-
-
 }
