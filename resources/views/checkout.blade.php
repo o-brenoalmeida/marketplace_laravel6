551955583dffc1017dadfc5d770d5037d332eadb
Breno Almeida <kbreno08@gmail.com>
2021-01-19 16:32:16 -0300

    Parcelamento dinâmico

--- resources/views/checkout.blade.php
+++ resources/views/checkout.blade.php
@@ -1,72 +1,141 @@
 @extends('layouts.front')
 
 @section('content')
-<div class="container">
-    <div class="col-md-6">
-        <div class="row">
-            <div class="col-md-12">
-                <h2>Dados para pagamento</h2>
-                <hr>
+    <div class="container">
+        <div class="col-md-6">
+            <div class="row">
+                <div class="col-md-12">
+                    <h2>Dados para pagamento</h2>
+                    <hr>
+                </div>
             </div>
-        </div>
-    <form action="" method="POST">
-        <div class="row">
-            <div class="form-group col-md-12">
-                <label>Número do Cartão <span class="brand"></span></label>
-                <input type="text" name="card_number"  class="form-control">
-            </div>
-        </div>
+            <form action="" method="POST">
+                <div class="row">
+                    <div class="form-group col-md-12">
+                        <label>Número do Cartão <span class="brand"></span></label>
+                        <input type="text" name="card_number" class="form-control">
+                        <input type="hidden" name="card_brand">
+                    </div>
+                </div>
 
-        <div class="row">
-            <div class="form-group col-md-4">
-                <label>Mês de Expiração</label>
-                <input type="text" name="card_month"  class="form-control">
-            </div>
-            <div class="form-group col-md-4">
-                <label>Ano de Expiração</label>
-                <input type="text" name="card_year"  class="form-control">
-            </div>
-        </div>
+                <div class="row">
+                    <div class="form-group col-md-4">
+                        <label>Mês de Expiração</label>
+                        <input type="text" name="card_month" class="form-control">
+                    </div>
+                    <div class="form-group col-md-4">
+                        <label>Ano de Expiração</label>
+                        <input type="text" name="card_year" class="form-control">
+                    </div>
+                </div>
 
-        <div class="row">
-            <div class="form-group col-md-5">
-                <label>Código de segurança</label>
-                <input type="text" name="card_cvv"  class="form-control">
-            </div>
-        </div>
+                <div class="row">
+                    <div class="form-group col-md-5">
+                        <label>Código de segurança</label>
+                        <input type="text" name="card_cvv" class="form-control">
+                    </div>
+                    <div class="col-md-12 installments form-group">
 
-        <button class="btn btn-success btn-lg">Efetuar Pagamento</button>
-    </form>
+                    </div>
+                </div>
+
+                <button class="btn btn-success btn-lg processCheckout">Efetuar Pagamento</button>
+            </form>
+        </div>
     </div>
-</div>
 @endsection
 
 @section('scripts')
-<script src="https://stc.sandbox.pagseguro.uol.com.br/pagseguro/api/v2/checkout/pagseguro.directpayment.js"></script>
+    <script src="https://stc.sandbox.pagseguro.uol.com.br/pagseguro/api/v2/checkout/pagseguro.directpayment.js"></script>
     <script>
-        const sessionId = '{{session()->get('pagseguro_session_code')}}';
+        const sessionId = '{{ session()->get('
+        pagseguro_session_code ') }}';
         PagSeguroDirectPayment.setSessionId(sessionId);
+
     </script>
 
     <script>
         let cardNumber = document.querySelector('input[name=card_number]');
         let spanBrand = document.querySelector('span.brand');
-        cardNumber.addEventListener('keyup', function(){
-            if(cardNumber.value.length >= 6){
+        cardNumber.addEventListener('keyup', function() {
+            if (cardNumber.value.length >= 6) {
                 PagSeguroDirectPayment.getBrand({
-                    cardBin: cardNumber.value.substr(0,6),
-                    success: function(res){
+                    cardBin: cardNumber.value.substr(0, 6),
+                    success: function(res) {
                         let imgFlag = `<img src="https://stc.pagseguro.uol.com.br/public/img/payment-methods-flags/68x30/${res.brand.name}.png">`;
                         spanBrand.innerHTML = imgFlag;
+                        document.querySelector('input[name=card_brand]').value = res.brand.name;
+
+                        getInstallments(40, res.brand.name);
                     },
-                    error: function(err){
-                        console.log(err);
+                    error: function(err) {
+                         console.log(err);
                     },
-                    complete: function(res){
-                        console.log('Complete' , res);
+                    complete: function(res) {
+                         console.log('Complete' , res);
                     }
                 });
             }
         });
+
+        let submitButton = document.querySelector('button.processCheckout');
+ 
+        submitButton.addEventListener('click', function(event){
+ 
+            event.preventDefault();
+ 
+            PagSeguroDirectPayment.createCardToken({
+                cardNumber:document.querySelector('input[name=card_number]').value,
+                brand:document.querySelector('input[name=card_brand]').value,
+                cvv:document.querySelector('input[name=card_cvv]').value,
+                expirationMonth:document.querySelector('input[name=card_month]').value,
+                expirationYear:document.querySelector('input[name=card_year]').value,
+                success:function(res){
+                    console.log(1);
+                },
+                error: function(err){
+                    console.log(err);
+                },
+                complete:function(res){
+                    console.log('Card',res);
+                }
+                });
+ 
+        });
+
+        function getInstallments(amount, brand) {
+            PagSeguroDirectPayment.getInstallments({
+                amount: amount,
+                brand: brand,
+                maxInstallmentNoInterest: 0, // o quanto de juros que você assume
+                success: function(res) {
+                    let selectInstallments = drawSelectInstallments(res.installments[brand]);
+                    document.querySelector('div.installments').innerHTML = selectInstallments;
+                },
+                error: function(err) {
+                    console.log(err);
+                },
+                complete: function(res) {
+                    // console.log(res);
+                },
+            })
+        }
+
+        function drawSelectInstallments(installments) {
+            let select = '<label>Opções de Parcelamento:</label>';
+
+            select += '<select class="form-control">';
+
+            for (let l of installments) {
+                select +=
+                    `<option value="${l.quantity}|${l.installmentAmount}">${l.quantity}x de R$${l.installmentAmount} - Total fica R$${l.totalAmount}</option>`;
+            }
+
+
+            select += '</select>';
+
+            return select;
+        }
+
     </script>
 @endsection
