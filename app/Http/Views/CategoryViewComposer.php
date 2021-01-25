5f3a3a3d0fd67cf5ead6c8ac09557743f83de550
Breno Almeida <kbreno08@gmail.com>
2021-01-25 11:58:30 -0300

    Uso de ViewComposer para categorias do menu

---
+++ app/Http/Views/CategoryViewComposer.php
@@ -0,0 +1,15 @@
+<?php
+
+namespace App\Http\Views;
+
+use App\Category;
+
+class CategoryViewComposer{
+    public function __construct(Category $category){
+        $this->category = $category;
+    }
+
+    public function compose($view){
+        return $view->with('categories', $this->category->all(['name', 'slug']));
+    }
+}
